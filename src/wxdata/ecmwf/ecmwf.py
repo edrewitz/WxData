4e73d2252257cd79d25fedbcfbf81edb30f613cd
edrewitz <drewitzwx@gmail.com>
2026-03-03 20:29:55 -0800

    update docstring

--- src/wxdata/ecmwf/ecmwf.py
+++ src/wxdata/ecmwf/ecmwf.py
@@ -1803,7 +1803,7 @@ def ecmwf_aifs_ens(final_forecast_hour=360,
                       41, 42, 43, 44, 45, 46, 47, 48, 49, 50]):
 
     """
-    This function scans for the latest ECMWF IFS dataset. If the dataset on the computer is old, the old data will be deleted
+    This function scans for the latest ECMWF AIFS Ensemble dataset. If the dataset on the computer is old, the old data will be deleted
     and the new data will be downloaded. 
     
     1) final_forecast_hour (Integer) - Default = 360. The final forecast hour the user wishes to download. The ECMWF IFS

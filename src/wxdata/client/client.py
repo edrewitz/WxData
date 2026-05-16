67b52435c1e967e764fefc03debd1214f97d29cb
edrewitz <drewitzwx@gmail.com>
2026-05-16 09:58:27 -0700

    update docstring

--- src/wxdata/client/client.py
+++ src/wxdata/client/client.py
@@ -881,9 +881,8 @@ def byte_range_request(grib_url,
     
     3) notifications (String) - Default='on'. Notification when a file is downloaded and saved to {path}
     
-    4) clear_recycle_bin (Boolean) - (Default=False in WxData >= 1.2.5) (Default=True in WxData < 1.2.5). When set to True, 
-        the contents in your recycle/trash bin will be deleted with each run of the program you are calling WxData. 
-        This setting is to help preserve memory on the machine. 
+    4) clear_recycle_bin (Boolean) - Default=False. When set to True, the contents in your recycle/trash bin 
+    will be deleted with each run of the program you are calling WxData. This setting is to help preserve memory on the machine. 
         
     Returns
     -------

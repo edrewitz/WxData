c5a0ff0946e5954f6dfe1e352bc12c1cf5eca8b7
edrewitz <drewitzwx@gmail.com>
2026-07-23 22:53:39 -0700

    Added `get_data_bounding_box()`

--- src/wxdata/airnow_api/observations.py
+++ src/wxdata/airnow_api/observations.py
@@ -15,14 +15,11 @@ from time import sleep as _sleep
 try:
     from datetime import(
         datetime,
-        timedelta,
         UTC
     )
 except Exception as e:
-    from datetime import(
-        datetime,
-        timedelta
-    )
+    from datetime import datetime
+    
 from wxdata.utils.api import df_to_csv as _df_to_csv
 
 try:
@@ -30,7 +27,6 @@ try:
 except Exception as e:
     now = datetime.utcnow()
     
-t1_hour = now - timedelta(hours=1)
 
 if now.hour >= 10:
     now_hour = now.hour
@@ -38,11 +34,7 @@ if now.hour >= 10:
 else:
     now_hour = f"0{now.hour}"
     
-if t1_hour.hour >= 10:
-    t1_hr = t1_hour.hour
-    
-else:
-    t1_hr = f"0{t1_hour.hour}"
+
     
 def _error_message():
     
@@ -139,6 +131,123 @@ def get_current_data_bounding_box(api_key,
                                   proxies=proxies,
                                   to_csv=to_csv,
                                   path=path)
+            
+            return df
+        else:
+            print(f"Another exception occurred\nHTTP Status Code: {response.status_code} Reason: {response.reason}")
+            _sys.exit(1)
+    else:
+        pass
+                
+    df = _pd.read_json(_StringIO(response.text))
+    
+    df = df.drop('Parameter', axis=1)
+    df = df.rename(columns={'Value':f'{parameter.upper()}'})
+    
+    df['time'] = _pd.to_datetime(df['UTC'])
+    df = df.drop('UTC', axis=1)
+        
+    if to_csv == True:
+        _df_to_csv(df,
+                    path,
+                    parameter)
+    
+    return df
+
+
+def get_data_bounding_box(api_key,
+                            start,
+                            end,
+                            parameter='pm25',
+                            western_bound=-124.205070,
+                            eastern_bound=-75.337882,
+                            southern_bound=28.716781,
+                            northern_bound=45.419415,
+                            proxies=None,
+                            to_csv=False,
+                            path=f"Air Now Data/{now.strftime('%Y_%m_%d')}_{now_hour}"):
+    
+    """
+    This function retrieves air-quality data from the airnow API.
+    
+    Required Arguments:
+    
+    1) api_key (String) - The API Key to access the data. 
+    
+        To get an API Key create a free account at: https://docs.airnowapi.org/
+        
+    2) start (String) - The start time in the following format: 'YYYY-mm-ddTHH'
+    
+    2) end (String) - The end time in the following format: 'YYYY-mm-ddTHH'
+        
+    Optional Arguments:
+    
+    1) parameter (String) - Default='pm25'.
+    
+        Parameters
+        ----------
+        
+        'pm25' - PM 2.5
+        'pm10' - PM 10
+        'ozone' - Ozone (O3)
+        'no2' - Nitrogen Dioxide (NO2)
+        'co' - Carbon Monoxide (CO)
+        'so2' - Sulfur Dioxide (SO2)
+        
+    2) western_bound (Float or Integer) - Default=-124.205070. 
+    
+    3) eastern_bound (Float or Integer) - Default=-75.337882. 
+    
+    4) southern_bound (Float or Integer) - Default=28.716781. 
+    
+    5) northern_bound (Float or Integer) - Default=45.419415. 
+    
+    6) proxies (dict or None) - Default=None. If the user is using a proxy server, the user must change the following:
+
+        proxies=None ---> proxies={
+                               'http':'http://your-proxy-address:port',
+                               'https':'http://your-proxy-address:port'
+                               }
+    
+    7) to_csv (Boolean) - Default=False. When set to True the data will be saved as a CSV file to {path} wth {filename}
+    
+    8) path (String) - The path where the CSV file is saved to.
+    
+    Returns
+    -------
+    
+    A Pandas.DataFrame of all the current air quality observations within the bounding box.     
+    """
+    
+    
+    if proxies == None:
+        response = _requests.get(f"https://www.airnowapi.org/aq/data/?"
+                                 f"startDate={start}&endDate={end}"
+                                 f"&parameters={parameter.upper()}"
+                                 f"&BBOX={western_bound},{southern_bound},{eastern_bound},{northern_bound}"
+                                 f"&dataType=B&format=application/json&API_KEY={api_key}")
+    else:
+        response = _requests.get(f"https://www.airnowapi.org/aq/data/?"
+                                 f"startDate={start}&endDate={end}"
+                                 f"&parameters={parameter.upper()}"
+                                 f"&BBOX={western_bound},{southern_bound},{eastern_bound},{northern_bound}"
+                                 f"&dataType=B&format=application/json&API_KEY={api_key}",
+                                 proxies=proxies)
+    if response.status_code != 200:
+        if response.status_code == 429:
+            _error_message()
+            _sleep(3600)
+            df = get_data_bounding_box(api_key,
+                                        start,
+                                        end,
+                                        parameter=parameter,
+                                        western_bound=western_bound,
+                                        eastern_bound=eastern_bound,
+                                        southern_bound=southern_bound,
+                                        northern_bound=northern_bound,
+                                        proxies=proxies,
+                                        to_csv=to_csv,
+                                        path=path)
             
             return df
         else:

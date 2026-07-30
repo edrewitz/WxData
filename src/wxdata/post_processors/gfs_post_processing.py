9226f2b6cc9df6137950e5465699d7dd136f8f90
Eric J. Drewitz <101157849+edrewitz@users.noreply.github.com>
2026-07-29 18:22:37 -0700

    Document required geographical bounds parameters
    Added documentation for geographical bounds parameters.

--- src/wxdata/post_processors/gfs_post_processing.py
+++ src/wxdata/post_processors/gfs_post_processing.py
@@ -41,6 +41,14 @@ def primary_gfs_post_processing(path,
     Required Arguments:
     
     1) path (String) - The path to the files.
+
+    2) western_bound (Float or Integer) - Default=-180. The western bound of the data needed. 
+
+    3) eastern_bound (Float or Integer) - Default=180. The eastern bound of the data needed.
+    
+    4) northern_bound (Float or Integer) - Default=90. The northern bound of the data needed.
+    
+    5) southern_bound (Float or Integer) - Default=-90. The southern bound of the data needed.
     
     Optional Arguments: None
     

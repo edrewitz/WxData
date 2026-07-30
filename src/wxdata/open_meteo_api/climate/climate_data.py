1338199e6302be49a426d5077a013dd24dd8dd74
edrewitz <drewitzwx@gmail.com>
2026-07-29 21:02:14 -0700

    Added HiRAM_SIT_HR and added errors for a 429 response code

--- src/wxdata/open_meteo_api/climate/climate_data.py
+++ src/wxdata/open_meteo_api/climate/climate_data.py
@@ -166,7 +166,8 @@ def fgoals_f3_h(latitude,
                         'temperature_2m_min',
                         'wind_speed_10m_mean',
                         'relative_humidity_2m_mean',
-                        'precipitation_sum'],
+                        'precipitation_sum',
+                        'shortwave_radiation_sum'],
             proxies=None,
             to_csv=False,
             path=f"Open Meteo Data/Climate/FGOALS_f3_H",
@@ -218,7 +219,8 @@ def fgoals_f3_h(latitude,
                                             'temperature_2m_min',
                                             'wind_speed_10m_mean',
                                             'relative_humidity_2m_mean',
-                                            'precipitation_sum']
+                                            'precipitation_sum',
+                                            'shortwave_radiation_sum']
    
                                             
                 The list of variables to choose from.
@@ -257,6 +259,131 @@ def fgoals_f3_h(latitude,
                              f"latitude={latitude}&longitude={longitude}"
                              f"&start_date={start_date}&end_date={end_date}"
                              f"&daily={','.join(variables)}&models=FGOALS_f3_H"
+                             f"&wind_speed_unit={wind_speed_units}"
+                             f"&precipitation_unit={precipitation_units}&temperature_unit={temperature_units}",
+                             proxies=proxies)
+        
+    _server_response(response)
+        
+    data = response.json()
+    
+    df = _json_to_pandas(data,
+                         field='daily')
+    
+    df['time'] = _pd.to_datetime(df['time'])
+        
+    if to_csv == True:
+        _df_to_csv(df,
+                   path,
+                   filename)
+    
+    return df
+
+def hiram_sit_hr(latitude,
+            longitude,
+            start_date='1950-01-01',
+            end_date='2050-12-31',
+            temperature_units='fahrenheit',
+            wind_speed_units='mph',
+            precipitation_units='inch',
+            variables=['temperature_2m_mean',
+                        'temperature_2m_max',
+                        'temperature_2m_min',
+                        'wind_speed_10m_mean',
+                        'relative_humidity_2m_mean',
+                        'precipitation_sum',
+                        'shortwave_radiation_sum'],
+            proxies=None,
+            to_csv=False,
+            path=f"Open Meteo Data/Climate/HiRAM_SIT_HR",
+            filename=f"HiRAM_SIT_HR.csv"):
+    
+    """
+    This function retrieves daily HiRAM_SIT_HR climate data from the Open-Meteo API for a given point of latitude/longitude.
+    
+    Required Arguments:
+    
+    1) latitude (Float or Integer) - Latitude in decimal degrees.
+    
+    2) longitude (Float or Integer) - Longitude in decimal degrees.
+    
+    Optional Arguments:
+    
+    1) start_date (String) - Default='1950-01-01'. Start date in the format of 'YYYY-mm-dd'. Record begins at 1950-01-01.
+    
+    2) end_date (String) - Default='2050-12-31'. End date in the format of 'YYYY-mm-dd'. Record ends at 2050-12-31.
+    
+    3) temperature_units (String) - Default='fahrenheit'. The units for temperature.
+    
+        Valid Temperature Units
+        -----------------------
+        
+        1) fahrenheit
+        2) celsius
+        
+    4) wind_speed_units (String) - Default='mph'. The units for wind speed. 
+    
+        Valid Wind Speed Units
+        ----------------------
+        
+        1) mph - miles per hour
+        2) kmh - kilometers per hour
+        3) ms - meters per second
+        4) kn - knots
+        
+    5) precipitation_units (String) - Default='inch'. The units for precipitation amounts.
+    
+        Valid Precipitation Units
+        -------------------------
+        
+        1) inch - inches
+        2) mm - millimeters
+        
+    6) variables (String List) - Default=['temperature_2m_mean',
+                                            'temperature_2m_max',
+                                            'temperature_2m_min',
+                                            'wind_speed_10m_mean',
+                                            'relative_humidity_2m_mean',
+                                            'precipitation_sum',
+                                            'shortwave_radiation_sum']
+   
+                                            
+                The list of variables to choose from.
+                
+    7) proxies (dict or None) - Default=None. If the user is using a proxy server, the user must change the following:
+
+        proxies=None ---> proxies={
+                               'http':'http://your-proxy-address:port',
+                               'https':'http://your-proxy-address:port'
+                               }
+    
+    8) to_csv (Boolean) - Default=False. When set to True the data will be saved as a CSV file to {path} with {filename}
+    
+    9) path (String) - The path where the CSV file is saved to.
+    
+    10) filename (String) - The filename for the CSV file.                     
+                    
+    Returns
+    -------
+    
+    A Pandas.DataFrame of the HiRAM_SIT_HR time series for point latitude/longitude. 
+    """
+    
+    if proxies == None:
+        response = _requests.get(f"https://climate-api.open-meteo.com/v1/climate?"
+                             f"latitude={latitude}&longitude={longitude}"
+                             f"&start_date={start_date}&end_date={end_date}"
+                             f"&daily={','.join(variables)}&models=HiRAM_SIT_HR"
+                             f"&wind_speed_unit={wind_speed_units}"
+                             f"&precipitation_unit={precipitation_units}&temperature_unit={temperature_units}")
+        
+        
+        
+    else:
+        response = _requests.get(f"https://climate-api.open-meteo.com/v1/climate?"
+                             f"latitude={latitude}&longitude={longitude}"
+                             f"&start_date={start_date}&end_date={end_date}"
+                             f"&daily={','.join(variables)}&models=HiRAM_SIT_HR"
                              f"&wind_speed_unit={wind_speed_units}"
                              f"&precipitation_unit={precipitation_units}&temperature_unit={temperature_units}",
                              proxies=proxies)
